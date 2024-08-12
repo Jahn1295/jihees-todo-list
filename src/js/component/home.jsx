30b420d03b7e11ad1797874646f7e845726a7c13
Jahn1295 <jiheeahn1222@gmail.com>
2024-08-12 23:37:24 +0000

    done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,48 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [todos, setTodos] = useState([]);
+	const [inputValue, setInputValue] = useState('');
+	const handleAddTodo = (e) => {
+		if (e.key === "Enter" && inputValue.trim() !== "") {
+			setTodos([...todos, inputValue.trim()]);
+			setInputValue('');
+		}
+	}
+	const handleDeleteTodo = (index) => {
+		setTodos(todos.filter((todo, i) => index !== i))
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="todoListBody">
+			<h1 className="title">My ToDo's List</h1>
+			<ul className="list-group">
+				<li className="list-group-item">
+					<input
+						type='text'
+						value={inputValue}
+						onKeyDown={handleAddTodo}
+						placeholder="What Needs to be Done?"
+						onChange={(e) => setInputValue(e.target.value)}
+					/>
+				</li>
+				{todos.length === 0 ? (
+					<li className="list-group-item">No tasks. Add a task.</li>
+
+				) : (
+					todos.map((todo, index) => (
+						<li className="list-group-item" key={index}>
+							<div className="list-group-item-todo" id="screen">
+								{todo}
+							</div>
+							<span className="x-container" onClick={() => handleDeleteTodo(index)}><i className="fa-solid fa-trash-can"></i></span>
+						</li>
+					))
+				)}
+			</ul>
+			<div>
+				{todos.length} {todos.length === 1 ? "item" : "items"} left
+			</div>
 		</div>
 	);
 };
